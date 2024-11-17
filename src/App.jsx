6f9d6c0d4fcc30a77650dae26be247932902c708
Ajay Singh <contact@ajaysingh.ca>
2024-11-16 22:47:01 -0500

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import "./style.css"
 
 export default function App() {
-  return 
+  return <form className="new-item_form">
+    <div className="form-row">
+    <label htmlFor="item">New item</label>
+    <input type="text" id="item"></input>
+
+    </div>
+  </form>
 }
