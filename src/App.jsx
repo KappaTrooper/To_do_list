f179927ae7beb5adafdf50b5be2d645591e3a61b
Ajay Singh <contact@ajaysingh.ca>
2024-11-16 23:24:22 -0500

    wrote toggle function to highlight selected list items

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,18 @@ export default function App() {
     setNewItem("")
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
   console.log(todos);
   return (
     <>
@@ -39,7 +51,8 @@ export default function App() {
         {todos.map(todo => {
           return <li key={todo.id}>
           <label>
-            <input type="checkbox" checked={todo.completed} />
+            <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)} />
             {todo.title}
           </label>
           <button className="btn btn-danger">Delete</button>
