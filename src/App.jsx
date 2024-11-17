8080a9e415bef995c2ecec84e990a837c1e84bc8
Ajay Singh <contact@ajaysingh.ca>
2024-11-16 22:54:35 -0500

    added full form and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import "./style.css"
+import { useState } from "react";
+import "./style.css";
 
 export default function App() {
-  return <form className="new-item_form">
-    <div className="form-row">
-    <label htmlFor="item">New item</label>
-    <input type="text" id="item"></input>
+  const [newItem, setNewItem] = useState("")
+  
+  return (
+    <>
+      <form className="new-item_form">
+        <div className="form-row">
+          <label htmlFor="item">New item</label>
+          <input type="text" id="item"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
 
-    </div>
-  </form>
+      <h1 className="header"> Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
